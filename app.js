32b2466fe4b7338debdb682d059b0ad47b369baa
Kugesz <kovacsgergo.algyo@gmail.com>
2023-11-07 20:16:09 +0100

    mostmar atveszem😘

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var randomTomb = []
 window.onload = function() {
     if (tomb.length == 9){
         document.getElementById("suffel").addEventListener("click", function(){
-            
+            initialBoard();
+            randomTomb = setRandomOrder(tomb);
+            renderBoard(randomTomb, matrixHtml);
         })
         matrixHtml = document.getElementById("matrix");
     }else{
@@ -17,11 +19,14 @@ function initialBoard(){
     randomTomb = []
 }
 
-function setRandomOrder(eredetiTomb, ujTomb){
-    t = eredetiTomb
-    for(let i = t.length; i >= 0; i--){
-        ujTomb.push(t[i]);
-        t.slice(i,1);
+function setRandomOrder(eredetiTomb){
+    var ujTomb = []
+    let t = eredetiTomb
+    for(let i = t.length - 1; i >= 0; i--){
+        let r = veletlenSzam(0,i);
+        console.log(r, t[r]);
+        ujTomb.push(t[r]);
+        t.slice(r,1);
     }
     return ujTomb;
 }
@@ -30,20 +35,21 @@ function getItem(tomb, index){
     return tomb[index];
 }
 
-function renderBoard(tomb, htmlHely){
-    s = `<div class="row justify-content-around">
-            <div class="col-3"></div>
-            <div class="col-3"></div>
-            <div class="col-3"></div>
-        </div>
-        <div class="row justify-content-around">
-            <div class="col-3"></div>
-            <div class="col-3"></div>
-            <div class="col-3"></div>
-        </div>
-        <div class="row justify-content-around">
-            <div class="col-3"></div>
-            <div class="col-3"></div>
-            <div class="col-3"></div>
-        </div>`
-}
+    function renderBoard(tomb, htmlHely){
+        let s = `<div class="row justify-content-around">`
+            for(let i = 0; i <3; i++){
+                for(let j = 0; j < 3; j++){
+                    let szam = getItem(tomb, j + (i*3));
+                    s += `<div class="col-3"><p>${szam}</p></div>`;
+                }
+                s += `</div>
+                    <div class="row justify-content-around">`;
+            }
+            s += `</div>`;
+        console.log(s);
+        matrixHtml.innerHTML = s;
+    }
+
+    function veletlenSzam(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
